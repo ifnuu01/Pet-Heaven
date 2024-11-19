1e1749ae04dddf10291f336370b1c7e8d225ac03
ifnuu01 <ifnuu01@gmail.com>
2024-11-19 10:51:18 +0800

    Menampah Payment-succes

---
+++ payment-success.php
@@ -0,0 +1,31 @@
+<?php
+
+    include 'template-user/header.php';
+?>
+<link rel="stylesheet" href="assets/css/payment-success.css">
+
+<div class="hero">
+        <div class="container-hasil">
+            <div class="logo">
+                <img src="assets/logo//centang.png" alt="">
+            </div>
+            <div class="title"><h1>Pembelian Berhasil Dilakukan</h1></div>
+            <div class="text-content"><p>Pesanan anda sedang menunggu konfirmasi dari pihak kami <br>silahkan tunggu notifikasi selanjutnya</p></div>
+            <div class="btn">
+                <button class="lihat-riwayat">
+                    Lihat Riwayat Pembelian
+                </button>
+                <button class="menu">
+                    Menu Utama
+                </button>
+            </div>
+        </div>
+    </div>
+
+    
+    <?php
+    include 'template-user/footer.php';
+    ?>
+    <script src="assets/js/kategori-dropdown.js"></script>
+    <script src="assets/js/profile-dropdown.js"></script>
+    <script src="assets/js/modal-confirm.js"></script>
